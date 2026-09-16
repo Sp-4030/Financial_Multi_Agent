0f497d52d594c71c4f4296a173e98766bb72515c
Sp-4030 <180026852+Sp-4030@users.noreply.github.com>
2026-09-16 17:27:21 +0530

    chroma db vector database created

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
+import chromadb
 from utils.pdf_parser import extract_text_from_pdf
 from utils.chunking import split_text
 from utils.embeddings import create_embeddings
-
 
 pdf_path = "data/seed_documents/Capgemini.pdf"
 
@@ -14,7 +14,22 @@ chunks = split_text(text)
 # 3. Chunks → Embeddings
 embeddings = create_embeddings(chunks)
 
-print("Total chunks:", len(chunks))
-print("Embedding shape:", embeddings.shape)
-print("First embedding:")
-print(embeddings[0])
+
+# 4. Create ChromaDB client
+client = chromadb.PersistentClient(path="vector_db")
+
+
+# 5. Create collection
+collection = client.get_or_create_collection(name="financial_documents")
+
+
+# 6. Store chunks + embeddings
+collection.add(
+    ids=[f"chunk_{i}" for i in range(len(chunks))],
+    documents=chunks,
+    embeddings=embeddings.tolist(),
+)
+
+
+print("Successfully stored in ChromaDB!")
+print("Total documents:", collection.count())
